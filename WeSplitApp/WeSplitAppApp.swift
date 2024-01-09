6922cad3f872af30f613e3a4ee4fa22c091b9345
Leandro Vansan de Morais <59117400+lvm5@users.noreply.github.com>
2024-01-09 18:19:32 -0300

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Leandro Morais on 09/01/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
